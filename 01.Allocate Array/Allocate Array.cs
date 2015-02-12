025b685e709a0c67bc88cf3435b807a058f8883e
Vasil Vasilev <vasil.bonchev.vasilev@gmail.com>
2015-02-13 00:05:23 +0200

    Arrays Homework

---
+++ 01.Allocate Array/Allocate Array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+//Write a program that allocates array of 20 integers and initializes each element by its index multiplied by 5.
+//Print the obtained array on the console.
+
+
+class TwentyIntegers
+{
+    static void Main()
+    {
+        int[] numbers = new int[20];
+        for (int index = 0; index < numbers.Length; index++)
+        {
+            numbers[index] = index * 5;
+            Console.WriteLine(numbers[index] + "..");
+        }
+    }
+}
+
